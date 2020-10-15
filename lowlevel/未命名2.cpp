8b70800b1472b1a65bcb958c59ac8bb84a5e4848
BeatsLeo <1059899674@qq.com>
2020-10-15 11:10:03 +0800

    commit lowlevel

---
+++ lowlevel/未命名2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+	int m; 
+	m=1+2+3; 
+	printf("I love three things in the world.\nThe sun,the moon,and you.\nThe sun is fo day,the moon is for night.\nAnd you are forever.");
+    printf("\n2*3=%d\n小数就是浮点数",m);
+    int a,b,c;
+    a=1;
+    b=2;
+    c=a+b;
+    printf("a+b=%d",c);
+ }
